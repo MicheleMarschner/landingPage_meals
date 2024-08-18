9749292375a2c164f2d58a53ef5172187ae71d92
Michele Marschner <michele.marschner@gmail.com>
2024-08-18 22:15:29 +0200

    finished landingpage

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-// Get the button:
-let mybutton = document.getElementById("myBtn");
+let scrollbutton = document.getElementById("scrollBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    scrollbutton.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    scrollbutton.style.display = "none";
   }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
 }
